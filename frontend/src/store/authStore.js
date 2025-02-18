b0a3a398cbd9e93ae15636e81e82c517fd8c6881
Billa Akshith <billaakshith@gmail.com>
2025-02-18 23:42:31 +0530

    sfj

--- frontend/src/store/authStore.js
+++ frontend/src/store/authStore.js
@@ -1,7 +1,7 @@
 import { create } from "zustand";
 import axios from "axios";
 
-const API_URL =  "https://auth-system-p8ow.onrender.com/"
+const API_URL =  "https://auth-system-p8ow.onrender.com/api/auth/check-auth"
  
 
 axios.defaults.withCredentials = true;
